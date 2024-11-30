a8f5607909a3bdc0552f4298521b0d40f6339ef9
kyle <kyle@kylelee.dev>
2024-11-30 15:49:17 -0500

    handle trailing slash from proxy

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -5,8 +5,8 @@ import { watchlistRouter } from "./watchlists";
 import { bidsRouter } from "./bids";
 const apiRouter = new OpenAPIHono();
 
-apiRouter.route("/auctions", auctionsRouter);
-apiRouter.route("/watchlists", watchlistRouter);
-apiRouter.route("/categories", categoriesRouter);
-apiRouter.route("/bids", bidsRouter);
+apiRouter.route("/auctions/", auctionsRouter);
+apiRouter.route("/watchlists/", watchlistRouter);
+apiRouter.route("/categories/", categoriesRouter);
+apiRouter.route("/bids/", bidsRouter);
 export { apiRouter };
